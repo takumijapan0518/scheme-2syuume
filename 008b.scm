75cfa14b7e58650554cac257bbae68637d88736a
takumijapan0518 <132754793+takumijapan0518@users.noreply.github.com>
2023-06-30 14:09:10 +0900

    Update 008b.scm

--- 008b.scm
+++ 008b.scm
@@ -1,7 +1,7 @@
 (define (fib2 l m n)
   (cond ((equal? n 0) l)
         ((equal? n 1) m)
-        (else (fib2 m (+ 1 m) (- n 1)))))
+        (else (fib2 m (+ l m) (- n 1)))))
 (define (fib n) (fib2 1 1 n))
 
 (time (fib 40))
